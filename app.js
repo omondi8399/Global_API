d4d0808226902f12243f8b5aecf610886c85c030
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-04 21:06:20 +0300

    added app file

---
+++ app.js
@@ -0,0 +1,62 @@
+const fs = require("fs")
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.status(200).send("Hello i am nft marketplace api")
+// })
+
+// app.get('/', (req, res) => {
+//     res
+//     .status(200)
+//     .json({ 
+//         message: "Hello i am nft marketplace api",
+//         api: "NFT Marketplace"
+//     })
+// })
+
+// app.post('/', (req, res) => {
+//     res.status(201).json({
+//         message: "Your Data"
+//     })
+// })
+
+//GET REQUEST
+const nfts = JSON.parse(
+    fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
+) 
+
+// console.log(nfts)
+
+app.get('/api/v1/nfts', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: nfts.length,
+        data: {
+            nfts,
+        }
+    })
+})
+
+//POST METHOD
+app.post("/api/v1/nfts", (req, res) => {
+    // console.log(req)
+    // console.log(req.body)
+
+    const newId = nfts[nfts.length -1] + 1
+    const newNFTs = Object.assign({ id: newId }, req.body)
+
+    nfts.push(newNFTs)
+
+    fs.writeFile(`${__dirname}/nft-data/data/nft-simple.json`, JSON.stringify(nfts), error)
+    res.send("POST NFT")
+})
+
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`App running on port ${port}.....`)
+})
