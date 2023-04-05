8fb7a2e32be9e772697241f88582d2fc13122dbe
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-05 10:47:13 +0300

    updated code

--- app.js
+++ app.js
@@ -115,13 +115,141 @@
 // })
 
 
-//----------PART 2
+// //----------PART 2
+
+// const fs = require("fs")
+// const express = require("express")
+// const morgan = require("morgan")
+
+// const app = express()
+// app.use(express.json())
+// app.use(morgan("dev"))
+
+// //CUSTOM MIDDLE WARE
+
+// app.use((req, res, next) => {
+//     console.log("Hey i am from middleware function")
+//     next()
+// })
+
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString()
+//     next()
+// })
+
+
+// //GET REQUEST
+// const nfts = JSON.parse(
+//     fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
+// ) 
+
+// const getAllNfts = (req, res) => {
+//     console.log(req.requestTime)
+//     res.status(200).json({
+//         status: "success",
+//         requestTime:req.requestTime,
+//         results: nfts.length,
+//         data: {
+//             nfts,
+//         }
+//     })
+// }
+// //POST METHOD
+// const createNFT = (req, res) => {
+  
+
+//     const newId = nfts[nfts.length -1].id + 1
+//     const newNFTs = Object.assign({ id: newId }, req.body)
+
+//     nfts.push(newNFTs)
+
+//     fs.writeFile(`${__dirname}/nft-data/data/nft-simple.json`, JSON.stringify(nfts), err => {
+//         res.status(201).json({
+//             status: "success",
+//             nft:newNFTs
+//         })
+//     })
+    
+// } 
+// //GET SINGLE NFT
+// const getSingleNft = (req, res) => {
+
+//     const id = req.params.id * 1
+//     const nft = nfts.find((el) => (el.id === id))
+
+ 
+//         if (!nft) {
+//         return res.status(404).json({
+//             status: "Fail",
+//             message: "Invalid ID"
+//         })
+//     }
+//     res.status(200).json({
+//         status: "success",
+//         data: {
+//             nft,
+//         }
+//     })
+// } 
+// //PATCH METHOD
+// const updateNFT = (req, res) => {
+
+//     if (req.params.id * 1 > nfts.length) {
+//         return res.status(404).json({
+//             status: "Fail",
+//             message: "Invalid ID"
+//         })
+//     }
+//     res.status(200).json({
+//         status: "success",
+//         data: {
+//             nft: "updating nft"
+//         }
+//     })
+// }
+// //DELETE METHOD
+// const deleteNFT = (req, res) => {
+
+//     if (req.params.id * 1 > nfts.length) {
+//         return res.status(404).json({
+//             status: "Fail",
+//             message: "Invalid ID"
+//         })
+//     }
+//     res.status(204).json({
+//         status: "success",
+//         message: null
+//     })
+// }
+// // app.get('/api/v1/nfts', getAllNfts )
+// // app.post("/api/v1/nfts", createNFT )
+// //  app.get("/api/v1/nfts/:id", getSingleNft )
+// // app.patch("/api/v1/nfts/:id", updateNFT )
+// // app.delete("/api/v1/nfts/:id", deleteNFT )
+
+
+//----------PART 3
 
 const fs = require("fs")
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
 app.use(express.json())
+app.use(morgan("dev"))
+
+//CUSTOM MIDDLE WARE
+
+app.use((req, res, next) => {
+    console.log("Hey i am from middleware function")
+    next()
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
+})
+
 
 //GET REQUEST
 const nfts = JSON.parse(
@@ -129,8 +257,10 @@ const nfts = JSON.parse(
 ) 
 
 const getAllNfts = (req, res) => {
+    console.log(req.requestTime)
     res.status(200).json({
         status: "success",
+        requestTime:req.requestTime,
         results: nfts.length,
         data: {
             nfts,
@@ -204,11 +334,8 @@ const deleteNFT = (req, res) => {
         message: null
     })
 }
-// app.get('/api/v1/nfts', getAllNfts )
-// app.post("/api/v1/nfts", createNFT )
-// app.get("/api/v1/nfts/:id", getSingleNft )
-// app.patch("/api/v1/nfts/:id", updateNFT )
-// app.delete("/api/v1/nfts/:id", deleteNFT )
+
+//ROUTER NFTs
 
 app.route("/api/v1/nfts").get(getAllNfts).post(createNFT)
 
