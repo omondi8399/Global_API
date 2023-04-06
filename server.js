535e1f5d7f55d502ad9e984be34b9dccdeb9bd89
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-06 12:30:15 +0300

    added files and folders

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const dotenv = require("dotenv")
 const app = require("./app")
 
 // console.log(app.get("env"))
+
+dotenv.config({ path: "./config.env" })
 // console.log(process.env)
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`App running on port ${port}.....`)
 })
