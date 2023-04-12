970dd791df377f9042a410c7bbc99749712879fa
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-12 18:29:12 +0300

    updated code

--- app.js
+++ app.js
@@ -389,7 +389,40 @@
 // app.use("/api/v1/nfts", nftsRouter)
 
 
-//----------PART 4
+// //----------PART 4
+
+// const express = require("express")
+// const morgan = require("morgan")
+
+// const nftsRouter = require("./routes/nftsRoute")
+// const usersRouter = require("./routes/usersRoute")
+
+// const app = express()
+// app.use(express.json())
+
+// // if (process.env.NODE_ENV === "development") {
+// //     app.use(morgan("dev"))
+// // }
+// app.use(morgan("dev"))
+// //SERVING TEMPLATE DEMO
+// app.use(express.static(`${__dirname}/nft-data/img`))
+
+// //CUSTOM MIDDLE WARE
+// app.use((req, res, next) => {
+//     console.log("Hey i am from middleware function")
+//     next()
+// })
+
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString()
+//     next()
+// })
+
+// app.use("/api/v1/users", usersRouter)
+// app.use("/api/v1/nfts", nftsRouter)
+
+
+//----------PART 5-----ERROR
 
 const express = require("express")
 const morgan = require("morgan")
