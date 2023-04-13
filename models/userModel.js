0bf9bea312cb32f87e1f92684a1a356d8deb611d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-13 15:33:56 +0300

    code update

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const bcrypt = require("bcryptjs")
 
 //name, email, photo, password, passwordConfirmed
 
@@ -36,6 +37,16 @@ const userSchema = new mongoose.Schema({
         }
      }
     
+})
+
+userSchema.pre("save", async function (next) {
+    //PASSWORD MODIFIED
+    if(!this.isModified("password")) return next()
+    //HASH PASSWORD
+    this.password = await bcrypt.hash(this.password, 12)
+    //DELETES CONFIRM PASSWORD
+    this.passwordConfirm = undefined
+    next()
 })
 
 const User = mongoose.model("User", userSchema)
