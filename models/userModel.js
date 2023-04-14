cb019ddb2fa1ee6ff156e0bf57e7b9331e6d78c3
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-14 11:46:20 +0300

    code update

--- models/userModel.js
+++ models/userModel.js
@@ -51,6 +51,13 @@ const userSchema = new mongoose.Schema({
     
 })
 
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password") || this.isNew) return next()
+
+    this.passwordChangedAt = Date.now() - 1000
+    next()
+})
+
 userSchema.pre("save", async function (next) {
     //PASSWORD MODIFIED
     if(!this.isModified("password")) return next()
