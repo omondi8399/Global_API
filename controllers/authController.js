ef3c26ff1b93c9cc51a3ca691832e20f5fabdbe6
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-04-14 16:05:18 +0300

    code update

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
 
+    
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000),
+        // secure: true,
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV="production") cookieOptions.secure = true
+
+    res.cookie("jwt", token, cookieOptions)
+
 
     res.status(statusCode).json({
         status: "success",
